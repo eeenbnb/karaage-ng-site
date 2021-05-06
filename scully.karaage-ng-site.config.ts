4ee96cab5ef172d719122577a016b80c46768960
eeenbnb <eee.kz.www@gmail.com>
2021-05-06 13:59:34 +0900

    update, scully

--- scully.karaage-ng-site.config.ts
+++ scully.karaage-ng-site.config.ts
@@ -1,11 +1,11 @@
 import { ScullyConfig } from '@scullyio/scully';
-import * as fs from 'fs'
-let items:any[] = JSON.parse(fs.readFileSync("src/assets/data/all.json",'utf8'));
+// import * as fs from 'fs'
+//let items:any[] = JSON.parse(fs.readFileSync("src/assets/data/all.json",'utf8'));
 
 export const config: ScullyConfig = {
   projectRoot: "./src",
   projectName: "karaage-ng-site",
-  outDir: './docs/static',
+  outDir: './docs-static',
   routes: {},
-  extraRoutes: items.map((v)=> "/article/" + v.path)
+  //extraRoutes: items.map((v)=> "/article/" + v.path)
 };
