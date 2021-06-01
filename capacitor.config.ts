359032321b4a1ba7710a6c285c236da79eda9ed4
eeenbnb <eee.kz.www@gmail.com>
2021-06-01 14:03:51 +0900

    update 2021-05-31

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'tokyo.karaage.sushi',
+  appName: 'karaage-ng-site',
+  webDir: 'docs',
+  bundledWebRuntime: false
+};
+
+export default config;
