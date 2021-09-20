266303f6f07593aea66334986494a94a2077d427
eeenbnb <eee.kz.www@gmail.com>
2021-09-20 10:21:52 +0900

    add article, 2021-09-20

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   analytics:{
     id:"G-HXVPXJTZLN"
-  }
+  },
+  apiRoot:"https://sushi.karaage.tokyo/"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ export const environment = {
   production: false,
   analytics:{
     id:""
-  }
+  },
+  apiRoot:""
 };
 
 /*
